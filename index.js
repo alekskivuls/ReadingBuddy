f605cf5d3283b9933598618bb69004af291df19f
RichCherng <richcherng@gmail.com>
2017-01-14 14:17:04 -0800

    Fixed Express module to correct version. Create basic http node server, with bootstrap template.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+
+const PORT = 8080;
+
+
+app.get('/', function(req, res){
+
+	var express = require('express');
+
+	app.use(express.static(path.join(_dirname)));
+	res.sendFile(path.join(_dirname, 'view/index.html'));
+});
+
+
+// Listen application request on port 8080
+http.listen(PORT, function(){
+	console.log('listening on *:' + PORT);
+})
