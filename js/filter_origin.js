a9ef745ae83fc98a05d7277b1fdc443211b4aca9
Cherie Woo <cheriejawoo@yahoo.com>
2017-01-14 19:47:45 -0800

    Paragraphs and filter with accuracy scoring.

---
+++ js/filter_origin.js
@@ -0,0 +1,25 @@
+//Run while reader is reading.
+
+var filter = function (raw_txt) {
+	return raw_txt.replace("-"," ").replace(/[^a-zA-Z ]/g, "").toLowerCase();
+};
+
+//return result will be passed to logic.js.
+
+//Calculate accuracy
+
+var cal_acc = function (original, result) {
+	//Given array of reds and yellow.
+	//original
+	var score = original.length;
+	for (error : result) {
+		if (error.color == 0) { //red
+			score = score - 1;
+			//saying extra words will dock you another red. 
+		} else {
+			//yellow
+			score = score - 0.5;
+		}
+	}
+	return score/original.length;
+}
